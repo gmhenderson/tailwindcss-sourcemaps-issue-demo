45e58324717ee5e90a0f573e4ff85ee67793ed49
Garrett <garrett@rocketmedia.com>
2018-03-29 13:32:48 -0700

    Create a working example against which to compare

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
 
 gulp.task('default', function () {
-  var postcss = require('gulp-postcss')
+  gulp.start(['tailwind','workingexample'])
+})
+
+gulp.task('tailwind', function () {
   var tailwindcss = require('tailwindcss')
 
-  return gulp.src('src/main.css')
-    .pipe(sourcemaps.init({ loadMaps: true }))
+  return gulp.src('src/tailwind.css')
+    .pipe(sourcemaps.init())
     .pipe(postcss([
       tailwindcss(),
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/'))
+})
+
+gulp.task('workingexample', function () {
+  return gulp.src('src/working.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+        require('postcss-easy-import')
     ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/'))
